5ef6a0f3d9fc78da40d750365878c4ce06fdea32
Neoz30 <enzo.marr30@gmail.com>
2024-08-02 12:21:41 +0200

    change player coordinate to tilemap and adapt graphic for it (change jump key to space)

--- game.cpp
+++ game.cpp
@@ -13,11 +13,12 @@ struct Player {
 
 class TileMap {
     public:
+        SDL_DisplayMode* display;
         int tiles[64][64];
         int tilesize = 40;
-        int cam_x = 0, cam_y = 0;
 
-        TileMap() {
+        TileMap(SDL_DisplayMode* dm) {
+            display = dm;
             for (int i = 0; i < 64; i++) {
                 for (int j = 0; j < 64; j++) {
                         tiles[i][j] = 0;
@@ -26,12 +27,17 @@ class TileMap {
                 }
         }
 
-        void pos_to_tilemap(int x, int y, int* result) {
+        void DisplayposToTilepos(int x, int y, int* result) {
             result[0] = x / tilesize;
-            result[1] = y / tilesize;
-        } 
+            result[1] = (display->h - y) / tilesize;
+        }
+        void TileposToDislpaypos(float x, float y, int* result) {
+            result[0] = (int)(x * tilesize);
+            result[1] = (int)(-y * tilesize + display->h);
+        }
 
-        void display(SDL_Renderer* renderer) {
+
+        void draw(SDL_Renderer* renderer) {
             SDL_Rect tile;
             tile.w = tilesize;
             tile.h = tilesize;
@@ -39,8 +45,10 @@ class TileMap {
                 for (int j = 0; j < 64; j++) {
 
                     if (tiles[i][j] > 0) {
-                        tile.x = tilesize*i - cam_x;
-                        tile.y = -tilesize*(j+1) + 1080 - cam_y;
+                        int tile_pos[2];
+                        TileposToDislpaypos((float)i, (float)j, tile_pos);
+                        tile.x = tile_pos[0];
+                        tile.y = tile_pos[1] - tilesize;
 
                         SDL_SetRenderDrawColor(renderer, 64, 128, 32, 255);
                         SDL_RenderFillRect(renderer, &tile);
@@ -61,30 +69,21 @@ int main(int argc, char* argv[])
     SDL_DisplayMode DM;
     SDL_GetCurrentDisplayMode(0, &DM);
 
-    float displayscale = 1;
-
-    int Width = DM.w * displayscale;
-    int Height = DM.h * displayscale;
-    auto midWidth = Width / 2;
-    auto midHeight = Height / 2;
-
-    int fps = 60;
+    int fps = DM.refresh_rate;
 
     SDL_Window* window = SDL_CreateWindow("SDL2 TimeScale Beta",
-        0, 0, Width, Height, 0);
+        0, 0, DM.w, DM.h, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-    TileMap map;
+    TileMap map(&DM);
     map.tiles[0][0] = 1;
 
     Player player;
-    player.rect.x = midWidth;
-    player.rect.y = midHeight;
-    player.rect.w = 75 * displayscale;
-    player.rect.h = 75 * displayscale;
+    player.rect.w = 80;
+    player.rect.h = 80;
 
-    player.x = midWidth;
-    player.y = midHeight;
+    player.x = 1;
+    player.y = 1;
     player.velx = 0;
     player.vely = 0;
     player.accx = 0;
@@ -117,20 +116,20 @@ int main(int argc, char* argv[])
         mouse = SDL_GetMouseState(&mouse_x, &mouse_y);
 
         if (mouse & SDL_BUTTON(1)) {
-            if (0 <= mouse_x && mouse_x < Width && 0 <= mouse_y && mouse_y < Height) {
+            if (0 <= mouse_x && mouse_x < DM.w && 0 <= mouse_y && mouse_y < DM.h) {
                 int tile_pos[2];
-                int mx = max(0, min(Width, mouse_x + map.cam_x));
-                int my = max(0, min(Height, Height - mouse_y + map.cam_y));
-                map.pos_to_tilemap(mx, my, tile_pos);
+                int mx = max(0, min(DM.w, mouse_x));
+                int my = max(0, min(DM.h, mouse_y));
+                map.DisplayposToTilepos(mx, my, tile_pos);
                 map.tiles[tile_pos[0]][tile_pos[1]] = 1;
             }
         }
         if (mouse & SDL_BUTTON(3)) {
-            if (0 <= mouse_x && mouse_x < Width && 0 <= mouse_y && mouse_y < Height) {
+            if (0 <= mouse_x && mouse_x < DM.w && 0 <= mouse_y && mouse_y < DM.h) {
                 int tile_pos[2];
-                int mx = max(0, min(Width, mouse_x + map.cam_x));
-                int my = max(0, min(Height, Height - mouse_y + map.cam_y));
-                map.pos_to_tilemap(mx, my, tile_pos);
+                int mx = max(0, min(DM.w, mouse_x));
+                int my = max(0, min(DM.h, mouse_y));
+                map.DisplayposToTilepos(mx, my, tile_pos);
                 map.tiles[tile_pos[0]][tile_pos[1]] = 0;
             }
         }
@@ -139,19 +138,19 @@ int main(int argc, char* argv[])
         player.accy = 0;
 
         // player in air
-        if (player.y-player.rect.h/2 <= 0) {
+        if (player.y <= 1) {
             player.onground = true;
         } else {
             player.onground = false;
         }
 
         // Move rect
-        if (player.onground && keys[SDL_SCANCODE_W]){player.accy += 2000;}
-        if (keys[SDL_SCANCODE_D]){player.x += 1200*dt;}
-        if (keys[SDL_SCANCODE_A]){player.x -= 1200*dt;}
+        if (player.onground && keys[SDL_SCANCODE_SPACE]){player.accy += 32;}
+        if (keys[SDL_SCANCODE_D]){player.x += 16*dt;}
+        if (keys[SDL_SCANCODE_A]){player.x -= 16*dt;}
 
         if (!player.onground) {
-            player.accy -= 98;
+            player.accy -= 2;
         }
 
         player.velx += player.accx;
@@ -160,28 +159,27 @@ int main(int argc, char* argv[])
         player.x += player.velx*dt;
         player.y += player.vely*dt;
 
-        if (player.y-player.rect.h/2 < 0) {
+        if (player.y-1 < 0) {
             player.vely = 0;
-            player.y = player.rect.h/2;
+            player.y = 1;
         }
-        if (player.y+player.rect.h/2 > Height) {
+        if (player.y+1 > 64) {
             player.vely = 0;
-            player.y = Height-player.rect.h/2;
+            player.y = 63;
         }
-        if (player.x+player.rect.w/2 > Width) {
+        if (player.x+1 > 64) {
             player.velx = 0;
-            player.x = Width-player.rect.w/2;
+            player.x = 63;
         }
-        if (player.x-player.rect.w/2 < 0) {
+        if (player.x-1 < 0) {
             player.velx = 0;
-            player.x = player.rect.w/2; 
+            player.x = 1; 
         }
 
-        /*map.cam_x += player.x - midWidth;
-        player.x = midWidth;*/
-
-        player.rect.x = (int)(player.x-player.rect.h/2);
-        player.rect.y = (int)(-player.y+Height-player.rect.h/2);
+        int display_player[2];
+        map.TileposToDislpaypos(player.x, player.y, display_player);
+        player.rect.x = (int)(display_player[0]-player.rect.w/2);
+        player.rect.y = (int)(display_player[1]-player.rect.h/2);
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
@@ -189,7 +187,7 @@ int main(int argc, char* argv[])
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderFillRect(renderer, &player.rect);
 
-        map.display(renderer);
+        map.draw(renderer);
 
         SDL_RenderPresent(renderer);
 
