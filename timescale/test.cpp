8bd74b7035cfae4cc199d9c05a72b62c97b3f48d
Neoz30 <enzo.marr30@gmail.com>
2024-12-21 10:13:58 +0100

    Tweak population crossover

--- timescale/test.cpp
+++ timescale/test.cpp
@@ -8,44 +8,31 @@ int operation(int value, int value2, int index) {
     switch (index) {
     case 0:
         return value | value2;
-        break;
     case 1:
         return value & value2;
-        break;
     case 2:
         return value ^ value2;
-        break;
     case 3:
         return ~value;
-        break;
     case 4:
         return value << value2;
-        break;
     case 5:
         return value >> value2;
-        break;
     case 6:
         return value;
-        break;
     case 7:
         return -value;
-        break;
     case 8:
         return value+value2;
-        break;
     case 9:
         return value-value2;
-        break;
     case 10:
         return value*value2;
-        break;
     case 11:
-        if (value2 == 0) return -1;
+        if (value2 == 0) return 0x7FFFFFFF;
         return value/value2;
-        break;
     default:
         return 0;
-        break;
     }
 }
 
@@ -54,11 +41,15 @@ class Individu {
         int parameters[16] = {0};
     
         void randomize() {
+            int r;
+            char sector;
             for (int i = 0; i < 16; i+=2) {
-                parameters[i] = rand() % 16;
-            }
-            for (int i = 1; i < 16; i+=2) {
-                parameters[i] = rand() % 11;
+                sector = i % 4;
+                if (sector == 0) {
+                    r = rand();
+                }
+                parameters[i] = (r >> (sector*8)) & 15;
+                parameters[i+1] = (r >> (sector*8+4)) & 15 % 11;
             }
         }
 
@@ -66,10 +57,11 @@ class Individu {
             bool taken[3][3] = {false};
 
             for (int i = 0; i < 9; i++) {
-                int x = operation(operation(i, parameters[0], parameters[1]), parameters[2], parameters[3]);
-                int y = operation(operation(i, parameters[4], parameters[5]), parameters[6], parameters[7]);
-                x = operation(operation(x, parameters[8], parameters[9]), parameters[10], parameters[11]);
-                y = operation(operation(y, parameters[12], parameters[13]), parameters[14], parameters[15]);
+                int x = operation(i, parameters[0], parameters[1]) + operation(i, parameters[4], parameters[5]);
+                int y = operation(i, parameters[2], parameters[3]) + operation(i, parameters[6], parameters[7]);
+                int sx = x;
+                x =  operation(x, parameters[8], parameters[9]) + operation(y, parameters[12], parameters[13]);
+                y =  operation(y, parameters[10], parameters[11]) + operation(sx, parameters[14], parameters[15]);
 
                 if (x < -1 || 1 < x || y < -1 || 1 < y) continue;
 
@@ -83,15 +75,16 @@ class Individu {
             int score = 0;
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
-                    if (taken[i][j]) score++; 
+                    if (taken[i][j]) score++;
                 }
             }
             return score;
         }
 
         void crossover(Individu ind1, Individu ind2) {
+            char r = rand() % 4;
             for (int i = 0; i < 16; i++) {
-                parameters[i] = (rand() % 2 == 0) ? ind1.parameters[i]: ind2.parameters[i];
+                parameters[i] = ((i+r) % 4 == 0 || (i-1+r) % 4) ? ind1.parameters[i]: ind2.parameters[i];
             }
         }
 
@@ -100,6 +93,17 @@ class Individu {
                 int p = rand() % 16;
                 int k = (p % 2 == 0) ? 16 : 11;
                 parameters[p] = rand() % k;
+            }
+        }
+
+        void show_order() {
+            for (int i = 0; i < 9; i++) {
+                int x = operation(operation(i, parameters[0], parameters[1]), parameters[2], parameters[3]);
+                int y = operation(operation(i, parameters[4], parameters[5]), parameters[6], parameters[7]);
+                x = operation(operation(x, parameters[8], parameters[9]), parameters[10], parameters[11]);
+                y = operation(operation(y, parameters[12], parameters[13]), parameters[14], parameters[15]);
+
+                cout << "(" << x << ", " << y << ")" << endl;
             }
         }
 };
@@ -128,6 +132,7 @@ int main() {
     int fitness_score[16] = {0};
 
     int gen = 0;
+    int max_fitness = 0;
     while (gen < 100000) {
         for (int i = 0; i < 16; i++) {
             fitness_score[i] = population[i].fitness();
@@ -135,28 +140,42 @@ int main() {
 
         selection(population, fitness_score);
 
+        if (fitness_score[15] > max_fitness) max_fitness = fitness_score[15];
         if (fitness_score[15] >= 6) break;
 
-        population[13].crossover(population[13], population[12]);
-        population[12].crossover(population[13], population[11]);
-        population[11].crossover(population[13], population[10]);
-        population[10].crossover(population[12], population[11]);
-        population[9].crossover(population[12], population[10]);
-        population[8].crossover(population[11], population[10]);
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i >= j) continue;
+                population[count+4].crossover(population[i+9], population[j+9]);
+                count++;
+            }
+        }
 
-        for (int i = 0; i < 8; i++) {population[i].randomize();}
-        for (int i = 8; i < 16; i++) {population[i].mutation();}
+        for (int i = 0; i < 4; i++) {population[i].randomize();}
+        for (int i = 4; i < 16; i++) {population[i].mutation();}
 
         gen++;
     }
-
+    cout << endl;
     cout << "Last Generation: " << gen << endl;
+    cout << "Best Fitness All Time: " << max_fitness << endl;
 
     Individu best = population[15];
-    cout << best.fitness() << endl;
-    cout << best.parameters[0] << ":" << best.parameters[1] << endl;
-    cout << best.parameters[2] << ":" << best.parameters[3] << endl;
-    cout << best.parameters[4] << ":" << best.parameters[5] << endl;
-    cout << best.parameters[6] << ":" << best.parameters[7] << endl;
+    cout << "Last Best Individu: " << best.fitness() << endl;
+    cout << endl;
+
+    cout << "Parameters X:" << endl;
+    cout << best.parameters[0] << ":" << best.parameters[1] << " ";
+    cout << best.parameters[2] << ":" << best.parameters[3] << " ";
+    cout << best.parameters[8] << ":" << best.parameters[9] << " ";
+    cout << best.parameters[10] << ":" << best.parameters[11] << endl;
+    cout << "Parameters Y:" << endl;
+    cout << best.parameters[4] << ":" << best.parameters[5] << " ";
+    cout << best.parameters[6] << ":" << best.parameters[7] << " ";
+    cout << best.parameters[12] << ":" << best.parameters[13] << " ";
+    cout << best.parameters[14] << ":" << best.parameters[15] << endl;
+
+    //best.show_order();
     return 0;
 }
